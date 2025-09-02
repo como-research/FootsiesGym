46f47d0f5d15e84979b4d259cc0399343540539e
chasemcd <chasecmcdonald@gmail.com>
2025-09-02 17:04:03 -0400

    use only port from dict

--- footsiesgym/footsies/footsies_env.py
+++ footsiesgym/footsies/footsies_env.py
@@ -111,12 +111,12 @@ class FootsiesEnv(env.MultiAgentEnv):
         }
 
     def _determine_port(self, config: dict[Any, Any] | env_context.EnvContext) -> int:
-        if isinstance(config, env_context.EnvContext):
-            return self._determine_port_from_env_context(config)
-        elif isinstance(config, dict):
+        # if isinstance(config, env_context.EnvContext):
+        #     return self._determine_port_from_env_context(config)
+        if isinstance(config, dict):
             return self._determine_port_from_dict(config)
         else:
-            raise TypeError("config must be a dict or env_context.EnvContext")
+            raise TypeError("config must be a dict")
 
     def _determine_port_from_dict(self, config: dict[Any, Any]):
         """Find an available port starting from a base port."""
